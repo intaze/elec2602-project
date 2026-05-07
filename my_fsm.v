4a18973a90d8c14a42b4d9b43dace3db55a4b8f8
Tina <tinaaep@gmail.com>
2026-05-07 13:30:22 +1000

    updated my_fsm to match idk

--- my_fsm.v
+++ my_fsm.v
@@ -1,3 +1,11 @@
-module my_fsm (function, a_in, g_in, g_out, addsub);
+module my_fsm (instruction, a_in, bus, g_out, addsub);
+    input [2:0] instruction;
+    input [15:0] a_in;
+    input [15:0] g_in;
+    output reg [15:0] g_out;
+    output reg addsub;
 
+    always @(*) begin
+
+    end
 endmodule
