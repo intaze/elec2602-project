6abe75885e92659bff405032af874f8c2a9d3127
Tina <tinaaep@gmail.com>
2026-05-07 13:23:32 +1000

    changed everything to 16 bits oof

--- my_alu.v
+++ my_alu.v
@@ -1,8 +1,8 @@
 module my_alu (a,bus,addsub,g);
-    input [3:0] a;
-    input [3:0] bus;
+    input [15:0] a;
+    input [15:0] bus;
     input addsub;
-    output reg [3:0] g;
+    output reg [15:0] g;
 
     always @(*) begin
         if (addsub==1'b1) begin
--- tri-buf.v
+++ tri-buf.v
@@ -1,7 +1,7 @@
 module tri_buf(a,b,enable);
 
-input [3:0] a;
-output [3:0] reg b;
+input [15:0] a;
+output [15:0] reg b;
 input enable;
 
 always @(enable or a) begin
